4791a99cda28eb4e19aec17ec9c75360cf1c871b
IGkougkousis <iraklisgougousis@protonmail.com>
2020-07-19 17:03:25 +0300

    Add list of places to board

--- src/model/board.ts
+++ src/model/board.ts
@@ -1,1 +1,18 @@
-export class Board {}
+import { Place } from "./place";
+
+export class Board {
+  public readonly places: Place[];
+
+  constructor() {
+    this.places = [];
+    this.initliazePlaces();
+  }
+
+  private initliazePlaces() {
+    for (let i = 0; i < 15; i++)
+      for (let j = 0; j < 15; j++) {
+        let place = new Place({ x: i, y: j });
+        this.places.push(place);
+      }
+  }
+}
--- tests/board.test.ts
+++ tests/board.test.ts
@@ -1,8 +1,19 @@
 import { Board } from "../src/model/board";
 
 describe("Board", function () {
-  it("should compile", function () {
+  it("should be created", function () {
     let board = new Board();
     expect(board).toBeTruthy();
+  });
+
+  it("should have a collection of places", function () {
+    let board = new Board();
+    expect(board.places).not.toBeNull();
+    expect(board.places.length).not.toBeNull();
+  });
+
+  it("should have 225 places", function () {
+    let board = new Board();
+    expect(board.places.length).toBe(225);
   });
 });
